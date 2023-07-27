d7bdd01b741b56b13edeb246b8eb233550dd46e5
ShekharchandraR <46771045+ShekharchandraR@users.noreply.github.com>
2023-07-27 17:42:11 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-    access_key = "AKIAQKOTMEQQ6R5MJSUD"
-    secret_key = "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"
+    access_key = "AKIAQKOTMEQQ3Y6KLQDJ"
+    secret_key = "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"
     region = "us-east-1"
 }
 
@@ -8,5 +8,5 @@ resource "aws_instance" "ec2_instance" {
     ami = "ami-053b0d53c279acc90"
     subnet_id = "subnet-04d543d667a79977f"
     instance_type = "t2.micro"
-    key_name = "sass"
-} 
+    key_name = "sass" 
+}
