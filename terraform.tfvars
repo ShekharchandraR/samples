13e0a113eef58072ac22060d76349224245d3c93
ShekharchandraR <46771045+ShekharchandraR@users.noreply.github.com>
2023-07-27 13:06:59 +0530

    Add files via upload

---
+++ terraform.tfvars
@@ -0,0 +1,2 @@
+access_key = "AKIAQKOTMEQQ6R5MJSUD"
+secret_key = "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"
---
+++ variables.tf
@@ -0,0 +1,30 @@
+variable "access_key" {
+        description = "Access key to AWS console"
+}
+
+variable "secret_key" {
+        description = "Secret key to AWS console"
+}
+
+variable "instance_name" {
+        description = "Name of the instance to be created"
+        default = "awsbuilder-demo"
+}
+
+variable "instance_type" {
+        default = "t2.micro"
+}
+
+variable "subnet_id" {
+        description = "The VPC subnet the instance(s) will be created in"
+        default = "subnet-04d543d667a79977f"
+}
+
+variable "ami_id" {
+        description = "The AMI to use"
+        default = "ami-053b0d53c279acc90"
+}
+
+variable "ami_key_pair_name" {
+        default = "sass"
+}
