4d34c253ed619ee87c6fd00c496f6df5bad931e5
ShekharchandraR <46771045+ShekharchandraR@users.noreply.github.com>
2023-07-27 14:47:15 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
-## provider "aws" {
-##    access_key = "${var.access_key}"
-##    secret_key = "${var.secret_key}"
-##    region = "us-east-1"
-## }
+provider "aws" {
+    access_key = "AKIAQKOTMEQQ6R5MJSUD"
+    secret_key = "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"
+    region = "us-east-1"
+}
 
 resource "aws_instance" "ec2_instance" {
     ami = "${var.ami_id}"
