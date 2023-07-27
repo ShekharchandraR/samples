ae2c3feca81f536b7b4c86daf2299d999f2238d0
ShekharchandraR <46771045+ShekharchandraR@users.noreply.github.com>
2023-07-27 18:23:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,12 +1,11 @@
 provider "aws" {
     access_key = "AKIAQKOTMEQQ3Y6KLQDJ"
     secret_key = "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"
-    region = "us-east-1"
+    region = "us-east-2"
 }
 
 resource "aws_instance" "ec2_instance" {
     ami = "ami-053b0d53c279acc90"
-    subnet_id = "subnet-04d543d667a79977f"
+    subnet_id = "subnet-0173576865f395551"
     instance_type = "t2.micro"
-    key_name = "sass" 
 }
